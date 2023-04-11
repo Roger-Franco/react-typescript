f1c8cebbe1adbc3d97f796d4379ff1458e1e2350
Roger Franco <rogerfrancogalt@gmail.com>
2023-04-10 23:08:12 -0400

    class

---
+++ src/components/ref/DomRef.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from 'react'
+
+export const DomRef = () => {
+  const inputRef = useRef<HTMLInputElement>(null!)
+  // const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current.focus()
+    // inputRef.current.focus?()
+  }, [])
+
+  return (
+    <div>
+      <input type="text" ref={inputRef} />
+    </div>
+  )
+}
